5a67c9ba645bc53302d64eb61533a37f4a65dd6c
babickajaVG <waleribg@gmail.com>
2020-04-11 17:37:00 +0300

    Add files via upload

---
+++ unit04/ConsoleApp.java
@@ -0,0 +1,10 @@
+package unit04;
+
+public class ConsoleApp {
+
+	public static void main(String[] args) {
+	System.out.println("Hello, console!");
+
+	}
+
+}
---
+++ unit04/task02.java
@@ -0,0 +1,45 @@
+package unit04;
+
+import java.util.Scanner;
+
+public class task02 {
+	public static void main(String arg[]) {
+
+		int a = GetA();
+		int b = GetB();
+		int s = GetSFromConsole(a, b);
+		int c = GetC(a, b);
+		GetAnswer(c, s);
+	}
+
+	public static int GetSFromConsole(int a, int b) {
+		System.out.println("Реши пример: " + a + " * " + b + " =");
+		Scanner scs = new Scanner(System.in);
+		int s = scs.nextInt();
+		return s;
+	}
+
+	public static int GetC(int a, int b) {
+		int c = a * b;
+		return c;
+	}
+
+	public static int GetA() {
+		int a = (int) (1 + Math.random() * 9);
+		return a;
+	}
+
+	public static int GetB() {
+		int b = (int) (1 + Math.random() * 9);
+		return b;
+	}
+
+	public static void GetAnswer(int c, int s) {
+		if (c == s) {
+			System.out.println("у ти умничка");
+		} else {
+			System.out.println("все очень плохо, парень");
+		}
+	}
+
+}
